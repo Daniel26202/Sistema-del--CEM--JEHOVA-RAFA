a925577004f3a3fb3b86961dbdd11acb28416aac
Daniel <Wbaez97@gamil.com>
2025-03-03 19:50:38 -0400

    inplementacion de recaptcha en inicio sesion, además, envio del código para el correo en recuperar password(aun no terminada la verificación)

--- src/controladores/ControladorIniciarSesion.php
+++ src/controladores/ControladorIniciarSesion.php
@@ -22,28 +22,75 @@ class ControladorIniciarSesion
 
         if (isset($_POST['validar'])) {
 
-            if ($_POST['usuario'] === '' or $_POST['password'] === '') {
+            // Clave secreta proporcionada por Google reCAPTCHA.
+            $claveSecreta = '6Le_rOgqAAAAAMEKli0Bp9zdh8i_haVpS008lTxc';
+            // Este token está incluido automáticamente en los datos del formulario bajo el nombre 'g-recaptcha-response'.
+            $token = $_POST['g-recaptcha-response'];
+            // Esto puede ser útil para agregar una capa adicional de seguridad.(opcional).
+            $ip = $_SERVER['REMOTE_ADDR'];
+            // Aquí es donde enviarás la solicitud para validar el token generado en el cliente.
+            $url = 'https://www.google.com/recaptcha/api/siteverify';
 
-                header("location: ?c=ControladorIniciarSesion/mostrarIniciarSesion&campos");
+            // Incluyen la clave secreta, el token del cliente y la dirección IP del usuario.
+            $datos = array(
+                'secret' => $claveSecreta,
+                // Clave secreta
+                'response' => $token,
+                // Token generado por el cliente
+                'remoteip' => $ip // Dirección IP del usuario (opcional)
+            );
 
-            } else {
+            // Configuración de la solicitud HTTP mediante método POST.
+            $options = array(
+                'http' => array(
+                    // Configura los encabezados para que la API sepa cómo procesar los datos.
+                    'header' => "Content-type: application/x-www-form-urlencoded\r\n",
+                    // Define el método de la solicitud como POST.
+                    'method' => 'POST',
+                    // Codifica los datos como una cadena de consulta para enviarlos en el cuerpo de la solicitud.
+                    'content' => http_build_query($datos)
+                )
+            );
 
-                $validar = $this->modelo->validarIniciarSesion($_POST['usuario'], $_POST['password']);
+            // Esto será utilizado para enviar la solicitud a la API de Google.
+            $context = stream_context_create($options);
+            // file_get_contents envía la solicitud HTTP y recibe la respuesta en formato JSON.
+            $response = file_get_contents($url, false, $context);
+            // Decodifica la respuesta JSON en un arreglo asociativo de PHP.
+            $result = json_decode($response, true);
 
-                if ($validar) {
+            // Verifica si la respuesta de la API indica éxito ('success' == true).
+            if ($result['success']) {
 
-                    session_start();
-                    $_SESSION['usuario'] = $_POST['usuario'];
-                    $_SESSION['rol'] = $validar['rol'];
-                    $_SESSION['id_usuario'] = $validar['id_usuario'];
-                    
-                    header("location: ?c=ControladorInicio/inicio");
+
+                if ($_POST['usuario'] === '' or $_POST['password'] === '') {
+
+                    header("location: ?c=ControladorIniciarSesion/mostrarIniciarSesion&campos");
 
                 } else {
 
-                    header("location: ?c=ControladorIniciarSesion/mostrarIniciarSesion&mensaje");
+                    $validar = $this->modelo->validarIniciarSesion($_POST['usuario'], $_POST['password']);
 
+                    if ($validar) {
+
+                        session_start();
+                        $_SESSION['usuario'] = $_POST['usuario'];
+                        $_SESSION['rol'] = $validar['rol'];
+                        $_SESSION['id_usuario'] = $validar['id_usuario'];
+
+                        header("location: ?c=ControladorInicio/inicio");
+
+                    } else {
+
+                        header("location: ?c=ControladorIniciarSesion/mostrarIniciarSesion&mensaje");
+
+                    }
                 }
+
+
+
+            } else {
+                    header("location: ?c=ControladorIniciarSesion/mostrarIniciarSesion&captcha");
             }
 
         }
