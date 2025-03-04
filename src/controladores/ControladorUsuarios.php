93557129295afff509be9bb09090c1ef830a0cc8
Daniel <Wbaez97@gamil.com>
2025-03-04 14:39:19 -0400

    se agrega la contraseña encriptada

--- src/controladores/ControladorUsuarios.php
+++ src/controladores/ControladorUsuarios.php
@@ -35,43 +35,43 @@ class ControladorUsuarios
     // editar usuario
     public function editarUsuario()
     {
-        
 
-            $resultadoDeUsuario = $this->modelo->validarUsuario($_POST['usuario']);
 
-           
-            // NOTA: Esto "&&" es "Y"
-            //se verifica si el usuario del input no es igual al usuario ya existente.  
-            if ($_POST["usuario"] != $_GET["usuarioDb"]) {
+        $resultadoDeUsuario = $this->modelo->validarUsuario($_POST['usuario']);
 
-                //verifica si el usuario es igual a la información de la base de datos.
-                if ($resultadoDeUsuario === "existeU") {
-                    header("location:?c=ControladorUsuarios/usuarios&error");
 
-                } else {
+        // NOTA: Esto "&&" es "Y"
+        //se verifica si el usuario del input no es igual al usuario ya existente.  
+        if ($_POST["usuario"] != $_GET["usuarioDb"]) {
 
-                    $this->modelo->updateUsuario($_POST["usuario"], $_POST["password"], $_POST["id_usuario"],$_FILES['imagenUsuario']["name"],$_FILES['imagenUsuario']['tmp_name']);
-
-                    header("location:?c=ControladorUsuarios/usuarios&editado");
-
-                }
-
-                //se verifica si el usuario del input es igual al usuario ya existente.  
-            } elseif ($_GET["usuarioDb"] == $_POST["usuario"]) {
-
-                $this->modelo->updateUsuario( $_POST["usuario"], $_POST["password"], $_POST["id_usuario"],$_FILES['imagenUsuario']["name"],$_FILES['imagenUsuario']['tmp_name']);
-
-                header("location:?c=ControladorUsuarios/usuarios&editado");
+            //verifica si el usuario es igual a la información de la base de datos.
+            if ($resultadoDeUsuario === "existeU") {
+                header("location:?c=ControladorUsuarios/usuarios&error");
 
             } else {
 
-                $this->modelo->updateUsuario($_POST["usuario"], $_POST["password"], $_POST["id_usuario"],$_FILES['imagenUsuario']["name"],$_FILES['imagenUsuario']['tmp_name']);
+                $this->modelo->updateUsuario($_POST["usuario"], $_POST["password"], $_POST["id_usuario"], $_FILES['imagenUsuario']["name"], $_FILES['imagenUsuario']['tmp_name']);
 
                 header("location:?c=ControladorUsuarios/usuarios&editado");
 
             }
 
-        
+            //se verifica si el usuario del input es igual al usuario ya existente.  
+        } elseif ($_GET["usuarioDb"] == $_POST["usuario"]) {
+
+            $this->modelo->updateUsuario($_POST["usuario"], $_POST["password"], $_POST["id_usuario"], $_FILES['imagenUsuario']["name"], $_FILES['imagenUsuario']['tmp_name']);
+
+            header("location:?c=ControladorUsuarios/usuarios&editado");
+
+        } else {
+
+            $this->modelo->updateUsuario($_POST["usuario"], $_POST["password"], $_POST["id_usuario"], $_FILES['imagenUsuario']["name"], $_FILES['imagenUsuario']['tmp_name']);
+
+            header("location:?c=ControladorUsuarios/usuarios&editado");
+
+        }
+
+
 
     }
 
@@ -87,33 +87,34 @@ class ControladorUsuarios
     public function registrarAdmin()
     {
 
-        
+        // Generamos la contraseña encriptada de la contraseña ingresada
+        $passwordEncrip = password_hash($_POST["password"], PASSWORD_BCRYPT);
 
-
-        $id_usuario =  $this->modelo->AgregarAdministrador($_POST["usuario"], $_POST["password"]); 
+        $id_usuario = $this->modelo->AgregarAdministrador($_POST["usuario"], $passwordEncrip, $_POST["Correo"]);
 
         $this->doctor->RegistrarAdmin($_POST["nacionalidad"], $_POST["cedula"], $_POST["nombre"], $_POST["apellido"], $_POST["telefono"], $_POST["Correo"], $id_usuario);
 
 
         header("location:?c=ControladorUsuarios/administradores&registrado");
-
     }
 
 
-    public function editarAdministrador(){
+    public function editarAdministrador()
+    {
 
-        $id_usuario =  $this->modelo->updateUsuario($_POST["usuario"], $_POST["password"],$_POST["id_usuario"], $_FILES["imagenUsuario"]["name"], $_FILES["imagenUsuario"]["tmp_name"]); 
+        $id_usuario = $this->modelo->updateUsuario($_POST["usuario"], $_POST["password"], $_POST["id_usuario"], $_FILES["imagenUsuario"]["name"], $_FILES["imagenUsuario"]["tmp_name"]);
 
         header("location: ?c=ControladorUsuarios/administradores&editado");
-        
+
     }
 
 
-    public function eliminarAdministrador() {
+    public function eliminarAdministrador()
+    {
         $this->modelo->eliminacionLogica($_POST["usuario"], $_POST["id_usuario"]);
 
         print_r($_POST);
-       header("location: ?c=ControladorUsuarios/administradores&eliminado");
+        header("location: ?c=ControladorUsuarios/administradores&eliminado");
 
 
     }
