7c1048d3756045f08da9f12df288ce7a91169f4f
DixonDanielGit <dixondanielbastias@gmail.com>
2025-03-14 09:05:47 -0430

    agregando la clase rutas para gestionar las rutas de controlador

--- index.php
+++ index.php
@@ -1,38 +1,7 @@
 <?php
 require_once "./vendor/autoload.php";
+use App\config\Rutas;
 $url = isset($_GET['url']) ? $_GET['url'] :  "IniciarSesion/mostrarIniciarSesion";
-$partes = explode("/", $url);
-$controlador = ucfirst($partes['0']);
-$metodo = isset($partes['1']) ? $partes['1'] : "IniciarSesion";
-$parametro = "";
 
-//si partes en la posicion 2 existe es por que se enviaron parametros
-if(isset($partes[2])){
-	for ($i=2; $i < count($partes); $i++) { 
-		$parametro .= $partes[$i].",";
-	}
-
-	//trim en php es para quitar el ultimo caracter en este caso es para quitar la ultima "," del parametro
-	$parametro = trim($parametro, ",");
-
-}
-
-$controlador = "Controlador".$controlador;
-
-$directorio = "src/controladores/" . $controlador . ".php";
-
-if (file_exists($directorio)) {
-	require_once $directorio;
-
-	$instancia = new $controlador();
-
-	if (method_exists($instancia, $metodo)) {
-		$instancia->$metodo($parametro);
-	} else {
-		echo "NO EXISTE EL METODO";
-	}
-
-} else {
-	echo "NO EXISTE EL CONTROLADOR";
-}
-
+$rutas = new Rutas($url);
+$rutas->gestionarRutas();
---
+++ src/config/Rutas.php
@@ -0,0 +1,54 @@
+<?php
+
+namespace App\config;
+
+class Rutas{
+
+    private $url,$partes,$controlador;
+
+
+    public function __construct($url) {
+        $this->url = $url;
+    }
+
+    
+
+    public function gestionarRutas(){
+        $this->partes = explode("/", $this->url);
+        $this->controlador = ucfirst($this->partes['0']);
+        $metodo = isset($this->partes['1']) ? $this->partes['1'] : "IniciarSesion";
+        $parametro = "";
+
+        //si partes en la posicion 2 existe es por que se enviaron parametros
+        if(isset($this->partes[2])){
+	        for ($i=2; $i < count($this->partes); $i++) { 
+		    $this->parametro .= $this->partes[$i].",";
+	        }       
+	        //trim en php es para quitar el ultimo caracter en este caso es para quitar la ultima "," del parametro
+	        $this->parametro = trim($this->parametro, ",");
+
+        }
+
+        $this->controlador = "Controlador".$this->controlador;
+
+        $directorio = "src/controladores/" . $this->controlador . ".php";
+
+        if (file_exists($directorio)) {
+            require_once $directorio;
+
+            $instancia = new $this->controlador();
+
+            if (method_exists($instancia, $metodo)){
+
+                $instancia->$metodo($parametro);
+            
+            }else echo "NO EXISTE EL METODO";
+            
+
+        } else echo "NO EXISTE EL CONTROLADOR";
+
+
+    }
+
+
+}
