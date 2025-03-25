83ec6c7ca6df397d4b8497df0e6774269059beff
Daniel <Wbaez97@gamil.com>
2025-03-25 12:41:20 -0400

    avances de dashboard

--- src/assets/js/dashboard.js
+++ src/assets/js/dashboard.js
@@ -1,15 +1,36 @@
 document.addEventListener("DOMContentLoaded", function () {
-  let ctx = document.getElementById("chartFluids").getContext("2d");
-  new Chart(ctx, {
-    type: "doughnut",
-    data: {
-      labels: ["Intracellular", "Extracellular", "Other"],
-      datasets: [
-        {
-          data: [50, 30, 20],
-          backgroundColor: ["#007bff", "#28a745", "#ffc107"],
+
+    // función async
+    const traerPromPacientes = async () => {
+        try {
+
+            // llamo la función buscar Insumos
+            let peticionInsumos = await fetch("/Sistema-del--CEM--JEHOVA-RAFA/Hospitalizacion/mostrarInsumos/" + valorI);
+
+            let resultadoInsu = await peticionInsumos.json();
+
+            //si se trae algo
+            if (resultadoInsu.length > 0) {
+            } else {
+            }
+
+        } catch (error) {
+            console.log("insumos lamentablemente Algo Salio Mal Por favor Intente Mas Tarde...");
+        }
+    }
+
+    let ctx = document.getElementById("chartFluids").getContext("2d");
+    new Chart(ctx, {
+        type: "doughnut",
+        data: {
+            labels: ["Intracellular", "Extracellular", "Other"],
+            datasets: [
+                {
+                    // pacientes mas visto por los doctores 
+                    data: [50, 30, 20],
+                    backgroundColor: ["#007bff", "#28a745", "#ffc107"],
+                },
+            ],
         },
-      ],
-    },
-  });
+    });
 });
--- src/config/Rutas.php
+++ src/config/Rutas.php
@@ -2,36 +2,39 @@
 
 namespace App\config;
 
-class Rutas{
+class Rutas
+{
 
-    private $url,$partes,$controlador;
+    private $url, $partes, $controlador;
 
 
-    public function __construct($url) {
+    public function __construct($url)
+    {
         $this->url = $url;
     }
 
-    
 
-    public function gestionarRutas(){
+
+    public function gestionarRutas()
+    {
         $this->partes = explode("/", $this->url);
         $this->controlador = ucfirst($this->partes['0']);
         $metodo = isset($this->partes['1']) ? $this->partes['1'] : "IniciarSesion";
         $parametro = "";
 
         //si partes en la posicion 2 existe es por que se enviaron parametros
-        if(isset($this->partes[2])){
-	        for ($i=2; $i < count($this->partes); $i++) { 
-		    $parametro .= $this->partes[$i].",";
-	        }       
-	        //trim en php es para quitar el ultimo caracter en este caso es para quitar la ultima "," del parametro
-	        $parametro = trim($parametro, ",");
+        if (isset($this->partes[2])) {
+            for ($i = 2; $i < count($this->partes); $i++) {
+                $parametro .= $this->partes[$i] . ",";
+            }
+            //trim en php es para quitar el ultimo caracter en este caso es para quitar la ultima "," del parametro
+            $parametro = trim($parametro, ",");
 
             $parametro = explode(",", $parametro);
 
         }
 
-        $this->controlador = "Controlador".$this->controlador;
+        $this->controlador = "Controlador" . $this->controlador;
 
         $directorio = "src/controladores/" . $this->controlador . ".php";
 
@@ -40,14 +43,16 @@ class Rutas{
 
             $instancia = new $this->controlador();
 
-            if (method_exists($instancia, $metodo)){
+            if (method_exists($instancia, $metodo)) {
 
                 $instancia->$metodo($parametro);
-            
-            }else echo "NO EXISTE EL METODO";
-            
 
-        } else echo "NO EXISTE EL CONTROLADOR";
+            } else
+                echo "NO EXISTE EL METODO";
+
+
+        } else
+            echo "NO EXISTE EL CONTROLADOR";
 
 
     }
