cd7001437dacf510592164a6a44115dd6db54d6b
DixonDanielGit <dixondanielbastias@gmail.com>
2025-03-27 22:19:15 -0400

    correccion de el responsive de datatable

--- src/assets/DataTable/datatables.js
+++ src/assets/DataTable/datatables.js
@@ -23971,6 +23971,7 @@
       filter.find("input").attr("id", "dt-search-" + __searchCounter);
 
 	  filter.find("input").attr("placeholder", "Buscar...");
+    filter.find("input").addClass('input-responsive')
     console.log(filter.find("input").attr("placeholder"));
 
 	  filter.find("input").focus(function(){
@@ -23988,7 +23989,7 @@
       // Add search button
       var searchButton = $("<button>")
         .attr("type", "button")
-        .addClass("btn btn-buscar")
+        .addClass("btn btn-buscar btn-search-datatable")
         .html('<svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-search" viewBox="0 0 16 16"><path d="M11.742 10.344a6.5 6.5 0 1 0-1.397 1.398h-.001l3.85 3.85a1 1 0 0 0 1.415-1.415l-3.85-3.85zm-5.442 1.398a5.5 5.5 0 1 1 0-11 5.5 5.5 0 0 1 0 11z"/></svg>')
         .on("click", function () {
           var val = filter.find("input").val();
--- src/vistas/head/footer.php
+++ src/vistas/head/footer.php
@@ -10,13 +10,11 @@
 	<script type="text/javascript" src="<?= $urlBase ?>../src/assets/intro/intro.min.js"></script>
 	<script type="text/javascript" src="<?= $urlBase ?>../src/assets/DataTable/jquery-3.7.1.js"></script>
 	<script type="text/javascript" src="<?= $urlBase ?>../src/assets/DataTable/datatables.js"></script>
+	<script type="text/javascript" src="<?= $urlBase ?>../src/assets/DataTable/modificacion.js"></script>
 	<script type="text/javascript" src="<?= $urlBase ?>../src/assets/js/chart.js"></script>
 
 	<script>
-		console.log("dt-search")
-
-		console.log($(".dt-input "))
-
+		
 		new DataTable('.example', {
 			
 		});
