7111b69c81d8cfdef4232611c9d2396cfac1c746
DixonDanielGit <dixondanielbastias@gmail.com>
2025-04-20 11:38:51 -0400

    se agregaron mas datos en equivalencias.php

--- src/config/equivalencias.php
+++ src/config/equivalencias.php
@@ -7,6 +7,11 @@ return [
 
     //Patlogias
     "patologias" => "consultar", "papeleraPatologias" => "consultar","registrarPatologia" => "guardar",
-    "eliminarPatologia" => "eliminar"
+    "eliminarPatologia" => "eliminar",
+
+    //Factura
+    "factura" => "consultar", "facturaCita" => "consultar","comprobante" => "consultar","mostrarPaciente" => "consultar","mostrarPacienteConCita" => "consultar","mostrarTodosLosServicios" => "consultar","mostrarTodosLosDoctores" => "consultar","mostrarPrecio" => "consultar","mostrarNombreTodosDoctores" => "consultar","nombreSegunIdC" => "consultar","nombreSegunIdD" => "consultar","insumosAjax" => "consultar","mostrarPDF" => "consultar","mostrarPDF2" => "consultar","mostrarPDF3" => "consultar","guardarFactura" => "guardar","guardarFacturaHospit" => "guardar",
+
+    //Citas
 
 ];
