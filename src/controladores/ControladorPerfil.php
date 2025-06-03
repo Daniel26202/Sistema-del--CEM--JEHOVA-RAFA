6be2b6770c56e31b1970af760646630491f536ef
DixonDanielGit <159038992+DixonDanielGit@users.noreply.github.com>
2025-06-03 12:59:17 -0400

    listo perfil edita

--- src/controladores/ControladorPerfil.php
+++ src/controladores/ControladorPerfil.php
@@ -2,18 +2,25 @@
 
 use App\modelos\ModeloPerfil;
 use App\modelos\ModeloPermisos;
-class ControladorPerfil{
+use App\modelos\ModeloBitacora;
+
+class ControladorPerfil
+{
 
 	private $modelo;
 	private $permisos;
+	private $bitacora;
 
 
-	function __construct(){
+	function __construct()
+	{
 		$this->modelo = new ModeloPerfil();
 		$this->permisos = new ModeloPermisos();
+		$this->bitacora = new ModeloBitacora();
 	}
-	
-	public function perfil($parametro){
+
+	public function perfil($parametro)
+	{
 		require_once './src/vistas/vistaPerfil/vistaPerfil.php';
 	}
 
@@ -22,10 +29,19 @@ class ControladorPerfil{
 		return $this->permisos->gestionarPermisos($id_rol, $permiso, $modulo);
 	}
 
+	//guardar perffil
+	public function guardar()
+	{
+		if (isset($_POST)) {
+			$edicion = $this->modelo->update($_POST["id_usuario"], $_POST["cedula"], $_POST["nombre"], $_POST["apellido"], $_POST["telefono"], $_POST["usuario"]);
 
-	
-
+			if ($edicion) {
+				// Guardar la bitacora
+				$this->bitacora->insertarBitacora($_POST["id_usuario"], "Perfil", "Ha modificado un perfil");
+				header("location: /Sistema-del--CEM--JEHOVA-RAFA/Perfil/perfil/editar");
+			} else {
+				header("location: /Sistema-del--CEM--JEHOVA-RAFA/Perfil/perfil/errorSistem");
+			}
+		}
+	}
 }
-
-
- ?>
