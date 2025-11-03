1b16fb1e5c0342b8fddd1c8cd3b044d4064ec59d
W. Daniel <129122054+Daniel26202@users.noreply.github.com>
2025-11-03 13:48:12 -0400

    bd actual

--- src/vistas/vistaPacientes/tablaHistorialSalud.php
+++ src/vistas/vistaPacientes/tablaHistorialSalud.php
@@ -10,14 +10,7 @@
             </tr>
         </thead>
         <tbody>
-            <?php foreach ($historial as $item): ?>
-                <tr>
-                    <td class="tex-center"><?= $item['cedula_paciente'] ?></td>
-                    <td class="text-center"><?= $item['nombre_paciente'] . ' ' . $item['apellido_paciente'] ?></td>
-                    <td class="text-center"><?= $item['estado_nuevo'] ?></td>
-                    <td class="text-center"><?= $item['fecha_cambio'] ?></td>
-                </tr>
-            <?php endforeach; ?>
+           
         </tbody>
     </table>
 </div>
