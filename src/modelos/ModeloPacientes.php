9648e03124f57cb8d226aba83c1acb775c131d60
= <159038992+DixonDanielGit@users.noreply.github.com>
2026-01-09 13:37:56 +0100

    setCedula corregido

--- src/modelos/ModeloPacientes.php
+++ src/modelos/ModeloPacientes.php
@@ -323,7 +323,7 @@ class ModeloPacientes extends ModelBase
 
 	public function setCedula($cedula)
 	{
-		if (!preg_match("/^[A-ZÁÉÍÓÚÑ][a-záéíóúñ]{2,}$/", $cedula)) {
+		if (!preg_match("/^([1-9]{1})([0-9]{7,8})$/", $cedula)) {
 			throw new \InvalidArgumentException("La cédula debe contener entre 7 y 8 dígitos.");
 		}
 		$this->cedula = $cedula;
