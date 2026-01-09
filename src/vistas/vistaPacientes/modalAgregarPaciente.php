0a5f394cc443ba74d137802b5660c5974a6d390b
W. Daniel <129122054+Daniel26202@users.noreply.github.com>
2026-01-09 13:08:51 -0400

    cambio de type, en el input cedula y telefono , a number

--- src/vistas/vistaPacientes/modalAgregarPaciente.php
+++ src/vistas/vistaPacientes/modalAgregarPaciente.php
@@ -31,7 +31,7 @@
                     <option value="E">E</option>
                   </select>
 
-                  <input class="form-control txt-custom input-validar" name="cedula" type="text" placeholder="Cédula del paciente">
+                  <input class="form-control txt-custom input-validar" name="cedula" type="number" placeholder="Cédula del paciente">
 
                   <span class="icono-der">
                     <svg class="check d-none" width="22" height="22" fill="currentColor" viewBox="0 0 16 16">
@@ -96,7 +96,7 @@
                       <path fill-rule="evenodd" d="M1.885.511a1.745 1.745 0 0 1 2.61.163L6.29 2.98c.329.423.445.974.315 1.494l-.547 2.19a.68.68 0 0 0 .178.643l2.457 2.457a.68.68 0 0 0 .644.178l2.189-.547a1.75 1.75 0 0 1 1.494.315l2.306 1.794c.829.645.905 1.87.163 2.611l-1.034 1.034c-.74.74-1.846 1.065-2.877.702a18.6 18.6 0 0 1-7.01-4.42 18.6 18.6 0 0 1-4.42-7.009c-.362-1.03-.037-2.137.703-2.877z" />
                     </svg>
                   </span>
-                  <input class="form-control txt-custom input-validar" name="telefono" type="text" placeholder="Teléfono del paciente">
+                  <input class="form-control txt-custom input-validar" name="telefono" type="number" placeholder="Teléfono del paciente">
                   <span class="icono-der">
                     <svg class="check d-none" width="22" height="22" fill="currentColor" viewBox="0 0 16 16">
                       <path d="M12.736 3.97a.733.733 0 0 1 1.047 0c.286.289.29.756.01 1.05L7.88 12.01a.733.733 0 0 1-1.065.02L3.217 8.384a.757.757 0 0 1 0-1.06.733.733 0 0 1 1.047 0l3.052 3.093 5.4-6.425a.247.247 0 0 1 .02-.022Z" />
