d72766701f9c29325bbeb4eae8eaf3c310bab7a3
DixonDanielGit <159038992+DixonDanielGit@users.noreply.github.com>
2026-02-19 11:13:53 -0400

    Se elimino una ruta inesesaria

--- src/vistas/vistaControl/vistaControl.php
+++ src/vistas/vistaControl/vistaControl.php
@@ -90,7 +90,6 @@
 <?php require_once './src/vistas/vistaControl/modalAgregarControl.php'; ?>
 <?php require_once './src/vistas/vistaControl/modalesSintomas.php'; ?>
 <?php require_once './src/vistas/vistaPacientes/modalAgregarPaciente.php'; ?>
-<?php require_once './src/vistas/vistaPacientes/modalAgregarPacienteCliente.php';?>
 
 <script type="module" src="<?= $urlBase ?>../src/assets/js/ajax/c.js"></script>
 <script type="text/javascript" src="<?= $urlBase ?>../src/assets/js/ayudaControl.js"></script>
