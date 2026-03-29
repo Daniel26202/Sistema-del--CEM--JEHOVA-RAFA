4700466187a5850d402d1ba2dbdc3872135b0b5e
DixonDanielGit <159038992+DixonDanielGit@users.noreply.github.com>
2026-03-28 20:36:21 -0400

    Se cambio la ubicacion de bitacora
    Ahora bitacora se encuentra en la seccion de mantenimiento del sistema

--- src/vistas/head/head.php
+++ src/vistas/head/head.php
@@ -94,13 +94,7 @@ if (!empty($parametro)) {
 						<path
 							d="M11.46.146A.5.5 0 0 0 11.107 0H4.893a.5.5 0 0 0-.353.146L.146 4.54A.5.5 0 0 0 0 4.893v6.214a.5.5 0 0 0 .146.353l4.394 4.394a.5.5 0 0 0 .353.146h6.214a.5.5 0 0 0 .353-.146l4.394-4.394a.5.5 0 0 0 .146-.353V4.893a.5.5 0 0 0-.146-.353L11.46.146zM5.496 6.033a.237.237 0 0 1-.24-.247C5.35 4.091 6.737 3.5 8.005 3.5c1.396 0 2.672.73 2.672 2.24 0 1.08-.635 1.594-1.244 2.057-.737.559-1.01.768-1.01 1.486v.105a.25.25 0 0 1-.25.25h-.81a.25.25 0 0 1-.25-.246l-.004-.217c-.038-.927.495-1.498 1.168-1.987.59-.444.965-.736.965-1.371 0-.825-.628-1.168-1.314-1.168-.803 0-1.253.478-1.342 1.134-.018.137-.128.25-.266.25h-.825zm2.325 6.443c-.584 0-1.009-.394-1.009-.927 0-.552.425-.94 1.01-.94.609 0 1.028.388 1.028.94 0 .533-.42.927-1.029.927z" />
 					</svg> Ayuda</a>
-				<a class="nav-link mx-5" href="/Sistema-del--CEM--JEHOVA-RAFA/Bitacora/bitacora" uk-tooltip="Bitacora"><svg xmlns="http://www.w3.org/2000/svg"
-						width="16" height="16" fill="currentColor" class="bi bi-question-octagon-fill  me-1"
-						viewBox="0 0 16 16">
-						<path
-							d="M7.068.727c.243-.97 1.62-.97 1.864 0l.071.286a.96.96 0 0 0 1.622.434l.205-.211c.695-.719 1.888-.03 1.613.931l-.08.284a.96.96 0 0 0 1.187 1.187l.283-.081c.96-.275 1.65.918.931 1.613l-.211.205a.96.96 0 0 0 .434 1.622l.286.071c.97.243.97 1.62 0 1.864l-.286.071a.96.96 0 0 0-.434 1.622l.211.205c.719.695.03 1.888-.931 1.613l-.284-.08a.96.96 0 0 0-1.187 1.187l.081.283c.275.96-.918 1.65-1.613.931l-.205-.211a.96.96 0 0 0-1.622.434l-.071.286c-.243.97-1.62.97-1.864 0l-.071-.286a.96.96 0 0 0-1.622-.434l-.205.211c-.695.719-1.888.03-1.613-.931l.08-.284a.96.96 0 0 0-1.186-1.187l-.284.081c-.96.275-1.65-.918-.931-1.613l.211-.205a.96.96 0 0 0-.434-1.622l-.286-.071c-.97-.243-.97-1.62 0-1.864l.286-.071a.96.96 0 0 0 .434-1.622l-.211-.205c-.719-.695-.03-1.888.931-1.613l.284.08a.96.96 0 0 0 1.187-1.186l-.081-.284c-.275-.96.918-1.65 1.613-.931l.205.211a.96.96 0 0 0 1.622-.434l.071-.286zM12.973 8.5H8.25l-2.834 3.779A4.998 4.998 0 0 0 12.973 8.5zm0-1a4.998 4.998 0 0 0-7.557-3.779l2.834 3.78h4.723zM5.048 3.967c-.03.021-.058.043-.087.065l.087-.065zm-.431.355A4.984 4.984 0 0 0 3.002 8c0 1.455.622 2.765 1.615 3.678L7.375 8 4.617 4.322zm.344 7.646.087.065-.087-.065z" />
-					</svg> Bitácora</a>
-
+				
 
 				<a href="#" class="mx-5" data-bs-toggle="modal" data-bs-target="#cerrar">
 					<img src="<?= $urlBase ?>../src/assets/images/img/icono-cerrar-sesion.svg" width="16" height="16" uk-svg
@@ -287,13 +281,38 @@ if (!empty($parametro)) {
 		</a>
 
 
-		<a class="nav-link d-flex  <?= $vistaActiva == 'mantenimiento' ? 'active' : '' ?>" id="menuMantenimiento" href="/Sistema-del--CEM--JEHOVA-RAFA/Mantenimiento/mantenimiento" uk-tooltip="Mantenimiento" id="menuReportesEstadisticos">
-			<div class="sb-nav-link-icon mx-2"><svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-wrench-adjustable" viewBox="0 0 16 16">
+
+
+
+		<a class="nav-link collapsed d-flex desplegableAyuda <?= $vistaActiva == 'mantenimiento' ? 'active' : '' ?>" href="#" data-bs-toggle="collapse" data-bs-target="#collapseMantenimiento" aria-expanded="false" aria-controls="collapseMantenimiento">
+			<div class="sb-nav-link-icon mx-2">
+				<svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-wrench-adjustable" viewBox="0 0 16 16">
 					<path d="M16 4.5a4.5 4.5 0 0 1-1.703 3.526L13 5l2.959-1.11q.04.3.041.61" />
 					<path d="M11.5 9c.653 0 1.273-.139 1.833-.39L12 5.5 11 3l3.826-1.53A4.5 4.5 0 0 0 7.29 6.092l-6.116 5.096a2.583 2.583 0 1 0 3.638 3.638L9.908 8.71A4.5 4.5 0 0 0 11.5 9m-1.292-4.361-.596.893.809-.27a.25.25 0 0 1 .287.377l-.596.893.809-.27.158.475-1.5.5a.25.25 0 0 1-.287-.376l.596-.893-.809.27a.25.25 0 0 1-.287-.377l.596-.893-.809.27-.158-.475 1.5-.5a.25.25 0 0 1 .287.376M3 14a1 1 0 1 1 0-2 1 1 0 0 1 0 2" />
-				</svg></div>
+				</svg>
+			</div>
 			Mantenimiento
+			<div class="sb-sidenav-collapse-arrow mx-2">
+				<svg xmlns="http://www.w3.org/2000/svg" width="10" height="10" fill="currentColor" class="bi bi-caret-down-fill" viewBox="0 0 16 16">
+					<path d="M7.247 11.14 2.451 5.658C1.885 5.013 2.345 4 3.204 4h9.592a1 1 0 0 1 .753 1.659l-4.796 5.48a1 1 0 0 1-1.506 0z" />
+				</svg>
+			</div>
 		</a>
+
+
+		<div class="collapse " id="collapseMantenimiento" aria-labelledby="headingCitas" data-bs-parent="#sidenavAccordion d-flex">
+			<nav class="sb-sidenav-menu-nested nav">
+				<a class="nav-link mx-5" href="/Sistema-del--CEM--JEHOVA-RAFA/Mantenimiento/mantenimiento" uk-tooltip="Perfil">
+					 Mantenimiento
+				</a>
+			
+				<a class="nav-link mx-5" href="/Sistema-del--CEM--JEHOVA-RAFA/Bitacora/bitacora" uk-tooltip="Bitacora">Bitácora</a>
+
+			</nav>
+		</div>
+
+
+
 
 	</div>
 
