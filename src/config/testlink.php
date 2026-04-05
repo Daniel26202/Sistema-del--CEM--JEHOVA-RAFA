b20683ba22b4b178d5f687921ef75d3639b2731a
W. Daniel <129122054+Daniel26202@users.noreply.github.com>
2026-04-05 15:47:17 -0400

    configuraciones viejas de testlink

---
+++ src/config/testlink.php
@@ -0,0 +1,8 @@
+// config/testlink_config.php
+<?php
+require_once '../vendor/autoload.php';
+
+define('TESTLINK_API_URL', 'http://localhost:8081/testlink/lib/api/xmlrpc/v1/xmlrpc.php');
+define('TESTLINK_API_KEY', '7fb692eba8ef73f3bbd9767fc7bb62f5');
+define('TEST_PROJECT_NAME', 'jftest');
+define('TEST_PLAN_NAME', 'Testeo');
