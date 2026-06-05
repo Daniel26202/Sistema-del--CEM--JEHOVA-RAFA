0f05d02e3aae5c0ee8259f4b26f8323bdf5efad1
Juanxrpana <jo.hw722@gmail.com>
2026-06-05 00:17:33 -0400

    cambio estrategico anti IA
    fuck ia en la bd y devolviendole la seguridad

--- database/bd.sql
+++ database/bd.sql
@@ -1,13 +1,12 @@
--- 1. Crear la base de datos SÓLO si no existe en el sistema
+
 CREATE DATABASE IF NOT EXISTS `bd` 
 DEFAULT CHARACTER SET utf8mb4 
 COLLATE utf8mb4_unicode_ci;
 
--- 2. Seleccionar la base de datos obligatoriamente para todo lo que viene abajo
+
 USE `bd`;
 
--- 3. Tu truco de seguridad para las llaves foráneas (para que no se tranque)
-SET FOREIGN_KEY_CHECKS = 0;
+
 
 -- phpMyAdmin SQL Dump
 -- version 5.2.1
